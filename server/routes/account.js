df243ec65d716fcdd67758707c0be62b6931a0e3
Jungmin Cha <ckwjdals90@gmail.com>
2017-05-23 02:00:22 -0700

    account router - signup api implemented

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,10 +1,52 @@
 import express from 'express';
+import Account from '../models/accoiunt';
 
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
-  /* to be implemented */
-  res.json({ success: true });
+  // Check Username Format
+  let usernameRegex = /^[a-z0-9]+$/;
+
+  if (!usernameRegex.test(req.body.username)) {
+    return res.status(400).json({
+      error: "BAD USERNAME",
+      code: 1
+    });
+  }
+
+  // Check Password Length
+  if (req.body.password.length < 4 || typeof req.body.password !== "string") {
+    return res.status(400).json({
+      error: "BAD PASSWORD",
+      code: 2
+    });
+  }
+
+  // Check Username Existance
+  Account.findOne({ username: req.body.username }, (err, exists) => {
+    if (err) throw err;
+    if (exists) {
+      return res.status(409).json({
+        error: "USERNAME EXISTS",
+        code: 3
+      });
+    }
+
+    // Create Account
+    let account = new Account({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    account.password = account.generateHash(account.password);
+
+    // Save in the Database
+    account.save( err => {
+      if (err) throw err;
+      return res.json({ success: true});
+    });
+
+  });
 });
 
 router.post('/signin', (req, res) => {
