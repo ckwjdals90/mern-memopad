7ed3b76c890491dfc35f752e0348db4e27846686
Jungmin Cha <ckwjdals90@gmail.com>
2017-05-23 00:36:58 -0700

    configured and applied morgan, mongodb, and express session

--- server/server.js
+++ server/server.js
@@ -4,10 +4,33 @@ import path from 'path';
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 
-const devPort = 4000;
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+import mongoose from 'mongoose';
+import session from 'express-session';
 
 const app = express();
 const PORT = 3000;
+const devPort = 4000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* MongoDB Connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to MongoDB Server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/ris_memopad');
+
+/* use session */
+app.use(session({
+  secret: 'ChaCha$04$21',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', express.static(path.join(__dirname, './../public')));
 
