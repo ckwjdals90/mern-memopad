39c9061f4f0e9ec4245a47847c6bd17e4e57ccd9
Jungmin Cha <ckwjdals90@gmail.com>
2017-05-23 18:33:58 -0700

    redux configured and applied using authentication, which is soon to be implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Container Components
 import { App, Home, Login, Register } from 'containers';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </App>
-  </Router>, rootElement
+  <Provider store={store}>
+    <Router>
+      <App>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </App>
+    </Router>
+  </Provider>, rootElement
 );
---
+++ src/reducers/authentication.js
@@ -0,0 +1,6 @@
+export default function authentication(state, action) {
+  if (typeof state === "undefined")
+    state = {};
+    /* to be implemented */
+    return state;
+}
